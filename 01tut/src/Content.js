48d0895e7c805f4b27f3e0056f1df601c7b243b6
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-04-13 13:52:33 +0200

    13.04

--- 01tut/src/Content.js
+++ 01tut/src/Content.js
@@ -5,9 +5,24 @@ const Content = () => {
 		return names[int];
 	};
 
+	const handleClick = () => {
+		console.log('Button clicked');
+	};
+
+	const handleClickTwo = (name) => {
+		console.log(`${name} was clicked`);
+	};
+
+	const handleClickThree = (e) => {
+		console.log(e.target.textContent);
+	};
+
 	return (
 		<main>
-			<p>Hello {handleNameChange()}</p>
+			<p onDoubleClick={handleClick}>Hello {handleNameChange()}</p>
+			<button onClick={handleClick}>Click it</button>
+			<button onClick={() => handleClickTwo('Radek')}>Click it</button>
+			<button onClick={(e) => handleClickThree(e)}>Click it</button>
 		</main>
 	);
 };
